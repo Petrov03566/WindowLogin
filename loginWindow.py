1e6bcdef2dcabda7feb41f39d09522d3bebae71a
NikitaPD <119868354+Petrov03566@users.noreply.github.com>
2023-05-19 23:31:03 +0500

    Update loginWindow.py

--- loginWindow.py
+++ loginWindow.py
@@ -1,19 +1,26 @@
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout
-from PyQt6.QtGui import QFont
+from PyQt6.QtCore import Qt
 import sys
-from mod import modlLogin, CaptchaLogin
+from modalsWindow import ModalLogin, CaptchaLogin
+
 
 class LoginWindow(QWidget):
-    def enterClicked(self, login, password):
-            if login == "a" and password == "b":
-                self.modal = modlLogin()
+    #  def enterClicked(self):
+    #         if login(self.login_edit.text(), self.password_edit.text()):
+    #             self.modal = ModalLogin()
+    #             self.modal.show()
+    #             self.close()
+    #         else:
+    #             self.captchaModal = CaptchaLogin()
+    #             self.captchaModal.show()
+    def enterClicked(self):
+            if self.login_edit.text() == "dalec" and self.password_edit.text() == "14":
+                self.modal = ModalLogin()
                 self.modal.show()
                 self.close()
             else:
                 self.captchaModal = CaptchaLogin()
                 self.captchaModal.show()
-
-    
     def __init__(self):
         super().__init__()
         #window settings
@@ -26,17 +33,15 @@ class LoginWindow(QWidget):
         self.password_edit = QLineEdit()
         self.enter_btn = QPushButton("Вход")
         #widgets settings
-        font = QFont("Open sans", pointSize=25)
-        font_btn = QFont("Open sans", pointSize=20)
-        self.login_lbl.setFont(font)
-        self.login_edit.setFont(font)
-        self.password_lbl.setFont(font)
-        self.password_edit.setFont(font)
-        self.enter_btn.setFont(font_btn)
+        self.login_lbl.setObjectName('LabelLogin')
+        self.password_lbl.setObjectName('LabelLogin')
+        self.login_edit.setObjectName('LabelLogin')
+        self.password_edit.setObjectName('LabelLogin')
+        self.enter_btn.setObjectName('PushButtonLogin')
         self.login_edit.setPlaceholderText("Логин")
         self.password_edit.setPlaceholderText("Пароль")
         #handler events
-        self.enter_btn.clicked.connect(lambda: self.enterClicked(self.login_edit.text(), self.password_edit.text()))
+        self.enter_btn.clicked.connect(self.enterClicked)
         #layout
         vbox = QVBoxLayout()
         vbox.addWidget(self.login_lbl)
@@ -45,6 +50,7 @@ class LoginWindow(QWidget):
         vbox.addWidget(self.password_edit)
         vbox.addWidget(self.enter_btn)
         self.setLayout(vbox)
+     
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
