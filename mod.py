21ddaf40d1ca4642ce800ba1d0cfcf72195369da
NikitaPD <119868354+Petrov03566@users.noreply.github.com>
2023-05-19 23:31:55 +0500

    Update mod.py

--- mod.py
+++ mod.py
@@ -1,15 +1,16 @@
 from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout, QStackedLayout, QLineEdit, QPushButton
 from PyQt6.QtCore import Qt, QTimer
-from PyQt6.QtGui import QFont, QPixmap
-import functools
-from testWidgets import StartWidget, TestWidget1, TestWidget2, TestWidget3, EndWidget
+import random as rnd
+import string
+from testWindow import StartWidget, TestWidget1, TestWidget2, TestWidget3, EndWidget
 
-class modlLogin(QWidget):
+class ModalLogin(QWidget):
+    results = [False, False, False]
     def __init__(self):
         super().__init__()
         #window settings
         self.setWindowTitle("Пройти тест")
-        self.setFixedSize(400, 200)
+        self.setFixedSize(150, 200)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
         #add stacks
         self.stackLayout = QStackedLayout()
@@ -18,92 +19,116 @@ class modlLogin(QWidget):
 
     def add_layouts(self):
         self.startWidget = StartWidget(lambda: self.close(), lambda:  self.enter_clicked())
-        self.testWidget1 = TestWidget1(lambda: self.next_clicked1())
-        self.testWidget2 = TestWidget2(lambda: self.next_clicked2())
-        self.testWidget3 = TestWidget3(lambda: self.next_clicked3())
-        self.endWidget = EndWidget()
+        self.testWidget1 = TestWidget1(lambda: self.next_clicked1(), lambda: self.back_clicked1())
+        self.testWidget2 = TestWidget2(lambda: self.next_clicked2(), lambda: self.back_clicked2())
+        self.testWidget3 = TestWidget3(lambda: self.next_clicked3(), lambda: self.back_clicked3())
         self.stackLayout.addWidget(self.startWidget)
         self.stackLayout.addWidget(self.testWidget1)
         self.stackLayout.addWidget(self.testWidget2)
         self.stackLayout.addWidget(self.testWidget3)
-        self.stackLayout.addWidget(self.endWidget)
 
     def next_clicked1(self):
         if self.testWidget1.rb1.isChecked():
-            self.stackLayout.setCurrentIndex(2)
+            self.results[0] = True
+        self.stackLayout.setCurrentIndex(2)
 
     def next_clicked2(self):
         if self.testWidget2.rb2.isChecked():
-            self.stackLayout.setCurrentIndex(3)
+            self.results[1] = True
+        self.stackLayout.setCurrentIndex(3)
 
     def next_clicked3(self):
         if self.testWidget3.rb3.isChecked():
-            self.stackLayout.setCurrentIndex(4)
+            self.results[2] = True
+        j = 0
+        text = None
+        for i in self.results:
+            if i:
+                j += 1
+        if j == 3:
+            text = "Все ответы правильны"
+        elif j == 0:
+            text = "Ничего не верно"
+        elif j == 1:
+            text = "Верно 1 из 3"
+        else:
+            text = "Верно 2 из 3"
+        self.endWidget = EndWidget(text, self.results)
+        self.stackLayout.addWidget(self.endWidget)
+        self.stackLayout.setCurrentIndex(4)
+
+    def back_clicked1(self):
+        self.stackLayout.setCurrentIndex(0)
+
+    def back_clicked2(self):
+        self.stackLayout.setCurrentIndex(1)
+
+    def back_clicked3(self):
+        self.stackLayout.setCurrentIndex(2)
 
     def enter_clicked(self):
         self.stackLayout.setCurrentIndex(1)
 
 class CaptchaLogin(QWidget):
-    isExit = False
     counter = 0
-    acess = True
-    def update_timer(self, timer): 
+    timer = 0
+    def update_timer(self): 
         self.attempt.setText(f"Ваша программа заблокирована на {self.total_seconds} секунд")
-        if self.total_seconds == timer:
+        if self.total_seconds == 0:
             self.attempt.setText("")
             self.timeq.stop()
-            self.acess = True
+            self.btn.setEnabled(True)
         self.total_seconds -= 1
 
-    def capthca_timer(self, timer):
-        self.total_seconds = timer
-        self.timeq = QTimer(self)
-        self.timeq.setInterval(1000)
-        self.timeq.timeout.connect(functools.partial(self.update_timer, 0))
+    def start_timer(self):
+        self.btn.setEnabled(False)
+        self.total_seconds = self.timer
         self.timeq.start()
 
-    def captcha_clicked(self, captcha):
-        if self.acess:
+    def capthca_timer(self):
+        self.timeq = QTimer()
+        self.timeq.setInterval(1000)
+        self.timeq.timeout.connect(self.update_timer)
+        self.start_timer()
+
+    def captcha_clicked(self):
+        if self.captcha_edit.text() == self.cap_text:
             self.timer = 0
-            if captcha == "2vyk":
-                self.isExit = True
-                self.close()
-            else:
-                self.counter += 1
-                if  self.counter == 2:
-                    self.timer = 3
-                elif  self.counter == 3:
-                    self.timer = 5
-                elif  self.counter > 3:
-                    self.timer = 10
-            if self.timer > 0:
-                self.acess = False
-                self.capthca_timer(self.timer)
-    def closeEvent(self, e):
-        if(not self.isExit):
-            e.ignore()
+            self.close()
+        else:
+            self.counter += 1
+            if self.counter == 2:
+                self.timer = 3
+            elif self.counter == 3:
+                self.timer = 5
+            elif self.counter > 3:
+                self.timer = 10
+        if self.timer > 0:
+            self.capthca_timer()
 
     def __init__(self):
         super().__init__()
         #window settings
+        self.setWindowFlag(Qt.WindowType.FramelessWindowHint)
         self.setWindowTitle("Подтвердите, что вы не робот")
         self.setFixedSize(450, 250)
         self.setWindowModality(Qt.WindowModality.ApplicationModal)
         #widgets
-        self.captcha = QLabel()
+        captcha_string = string.digits + string.ascii_lowercase
+        cap_text_list = rnd.sample(captcha_string, 4)
+        self.cap_text = ''.join(cap_text_list)
+        self.captcha = QLabel(f"<center>{self.cap_text}</center>")
         self.error = QLabel("<center>Введите текст с изображения</center>")
         self.captcha_edit = QLineEdit()
         self.attempt = QLabel("")
         self.btn = QPushButton("Ввести")
         #widget settings
-        font = QFont("Open sans", pointSize=15)
-        font_error = QFont("Open sans", pointSize=10)
-        self.error.setFont(font)
-        self.captcha_edit.setFont(font)
-        self.attempt.setFont(font_error)
-        self.captcha.setPixmap(QPixmap("./images/captcha.png"))
+        self.captcha.setObjectName("LabelCaptcha")
+        self.error.setObjectName("LabelModalWindow")
+        self.captcha_edit.setObjectName("LabelModalWindow")
+        self.attempt.setObjectName("LabelErrorModalWindow")
         #handler events
-        self.btn.clicked.connect(lambda: self.captcha_clicked(self.captcha_edit.text()))
+        self.btn.clicked.connect(self.captcha_clicked)
         #layout
         vbox = QVBoxLayout()
         vbox.addWidget(self.captcha)
